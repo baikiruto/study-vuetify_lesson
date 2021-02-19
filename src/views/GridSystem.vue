4602769809478e659f3dc8370cb24fc3b33a98b1
baikiruto <66161764+baikiruto@users.noreply.github.com>
2021-02-19 14:09:39 +0900

    Add baikiruto study

---
+++ src/views/GridSystem.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" md="4" lg="3">
+        <SaleGraph />
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3">
+        <SaleGraph />
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3">
+        <SaleGraph />
+      </v-col>
+      <v-col cols="12" sm="6" md="4" lg="3">
+        <SaleGraph />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import SaleGraph from "../components/SaleGraph";
+
+export default {
+  components: {
+    SaleGraph,
+  },
+};
+</script>
